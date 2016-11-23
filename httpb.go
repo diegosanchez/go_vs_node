58ba4583e44df8594d514111356aa36055415208
diegosanchez <diego.sanchez@mercadolibre.com>
2016-11-23 10:05:09 -0300

    Refactor

--- httpb.go
+++ httpb.go
@@ -9,17 +9,29 @@ import (
 	"os"
 )
 
-func main() {
-	var entry bson.M
+func connect() (*mgo.Collection, *mgo.Session) {
+	s, err := mgo.Dial("mongodb://127.0.0.1:27017")
 
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		panic(err)
 	}
 
-	c := session.DB("local").C("startup_log")
+	return s.DB("local").C("startup_log"), s
+}
 
-	err = c.Find(nil).One(&entry)
+func nextEntry(c *mgo.Collection, e *bson.M) error {
+	return c.Find(nil).One(e)
+}
+
+func disconnect(s *mgo.Session) {
+	s.Close()
+}
+
+func main() {
+	var entry bson.M
+	col, s := connect()
+	err := nextEntry(col, &entry)
 
 	if err != nil {
 		panic(err)
@@ -35,5 +47,5 @@ func main() {
 	json.Indent(&out, bs, "=", "\t")
 	out.WriteTo(os.Stdout)
 
-	session.Close()
+	disconnect(s)
 }
